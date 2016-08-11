963807ea9856b08793d93501aa1f184de0d826ab
weloxux <weloxux@glowbug.nl>
2016-08-11 11:25:06 +0200

    Add steamfix.sh

---
+++ home/dagon/steamfix.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Fixes Steam's library issues on Debian 8
+
+find ~/.steam/root/ \( -name "libgcc_s.so*" -o -name "libstdc++.so*" -o -name "libxcb.so*" \) -print -delete
+find ~/.steam/root/ \( -name "libgcc_s.so*" -o -name "libstdc++.so*" -o -name "libxcb.so*" \) -print -delete
+find ~/.local/share/Steam/ \( -name "libgcc_s.so*" -o -name "libstdc++.so*" -o -name "libxcb.so*" \) -print -delete
